619ff2e71f28520951f2be42d666d2149fd9d7f9
Lauren Matthews <126510096+laurenmatt14151@users.noreply.github.com>
2024-02-19 12:56:55 -0500

    Update

--- task_manager.py
+++ task_manager.py
@@ -1,1 +1,7 @@
-t
+tasks = []
+
+#add a task
+def add_task():
+    name = input("Enter task name: ")
+    description = input("Enter task description: ")
+    tasks.append({"name": name, "description": description, "completed": False})
